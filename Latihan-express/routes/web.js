0fee7592b0bb83e72038fe53cafad122b4d2779c
stevenlimya <stevenlimya@gmail.com>
2025-09-20 18:45:15 +0700

    latihan 20 september

---
+++ Latihan-express/routes/web.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const web = express.Router()
+
+web.get('/', (req, res) =>{
+    res.render('index')
+
+})
+
+web.get('/:username', (req, res) =>{
+    res.render('public-profile')
+
+})
+
+export default web
